6b4081104e7f36ce02a8aa10a24c3b80601305f4
hagarmhmd3 <104468433+hagarmhmd3@users.noreply.github.com>
2023-05-26 15:52:57 +0300

    Add files via upload

---
+++ MyTools.cshtml.cs
@@ -0,0 +1,139 @@
+using DentistPortal_Client.DTO;
+using DentistPortal_Client.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace DentistPortal_Client.Pages.DoctorPages.Tools
+{
+    public class MyToolsModel : PageModel
+    {
+        private IHttpClientFactory _httpClient;
+        IConfiguration config = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json")
+              .AddEnvironmentVariables()
+              .Build();
+        [TempData]
+        public string Msg { get; set; } = String.Empty;
+        [TempData]
+        public string Status { get; set; } = String.Empty;
+        public List<Tool> Tools = new();
+        public string[] Pictures { get; set; }
+        public Tool tool1;
+
+        public MyToolsModel(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task OnGet()
+        {
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(HttpContext.Session.GetString("Token"));
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
+            try
+            {
+                var request = await client.GetStringAsync($"/api/display-my-tools/{jwt.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value}");
+                if (request is not null)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+                    };
+                    Tools = JsonSerializer.Deserialize<List<Tool>>(request, options);
+                }
+                else
+                {
+                    Msg = request.ToString();
+                    Status = "error";
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                if (ex.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    LoginModel loginModel = new LoginModel(_httpClient);
+                    await loginModel.GetNewToken(HttpContext);
+                    await OnGet();
+                }
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+                Status = "error";
+            }
+        }
+        public async Task<IActionResult> OnPostDelete(Guid id)
+        {
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
+            var request = await client.DeleteAsync($"api/delete-Tool/{id}");
+            if (request.IsSuccessStatusCode)
+            {
+                Msg = "Successfully Deleted !";
+                Status = "success";
+                return RedirectToPage("");
+            }
+            else
+            {
+                Msg = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Status = "error";
+                return RedirectToPage("");
+            }
+        }
+
+        public async Task<IActionResult> OnPostEdit(ToolDto toolDto, Guid id)
+        {
+            var client = _httpClient.CreateClient();
+            client.BaseAddress = new Uri(config["BaseAddress"]);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
+            var multipartContent = new MultipartFormDataContent();
+            multipartContent = await MappingContent(multipartContent, toolDto);
+            var request = await client.PutAsync($"api/edit-tool/{id}", multipartContent);
+            if (request.IsSuccessStatusCode)
+            {
+                Msg = "Edited successfully!";
+                Status = "success";
+                return RedirectToPage("");
+            }
+            else
+            {
+                Msg = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Status = "error";
+                return RedirectToPage("", new { id });
+            }
+        }
+
+        private async Task<MultipartFormDataContent> MappingContent(MultipartFormDataContent multipartFormDataContent, ToolDto toolDto)
+        {
+            multipartFormDataContent.Add(new StringContent(toolDto.ToolName, Encoding.UTF8, MediaTypeNames.Text.Plain), "ToolName");
+            multipartFormDataContent.Add(new StringContent(toolDto.Description, Encoding.UTF8, MediaTypeNames.Text.Plain), "Description");
+            multipartFormDataContent.Add(new StringContent(toolDto.SellerLocation, Encoding.UTF8, MediaTypeNames.Text.Plain), "SellerLocation");
+            multipartFormDataContent.Add(new StringContent(toolDto.ContactNumber, Encoding.UTF8, MediaTypeNames.Text.Plain), "ContactNumber");
+            multipartFormDataContent.Add(new StringContent(toolDto.ToolPrice.ToString(), Encoding.UTF8, MediaTypeNames.Text.Plain), "ToolPrice");
+            multipartFormDataContent.Add(new StringContent(toolDto.SellerIdDoctor.ToString(), Encoding.UTF8, MediaTypeNames.Text.Plain), "SellerIdDoctor");
+            if (!string.IsNullOrEmpty(toolDto.ToolStatus))
+                multipartFormDataContent.Add(new StringContent(toolDto.ToolStatus, Encoding.UTF8, MediaTypeNames.Text.Plain), "ToolStatus");
+            if (toolDto.Pictures.Count > 0)
+            {
+                foreach (var file in toolDto.Pictures)
+                {
+                    var fileContent = new StreamContent(file.OpenReadStream());
+                    fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse(file.ContentType);
+                    multipartFormDataContent.Add(fileContent, "Pictures", file.FileName);
+                }
+            }
+            return multipartFormDataContent;
+        }
+    }
+}
